d912c10bd7b5dbbe97ca5fb198b993954f8d036a
David Alves <david27alves@gmail.com>
2019-10-25 00:26:59 -0300

    Agendamento

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var configRouter = require('./routes/configuracao');
+var agendRouter = require('./routes/agendamento');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/configuracao', configRouter);
+app.use('/agendamento', agendRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/configuracao.js
+++ routes/configuracao.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	res.render('configuracao', { title: 'Configuração | UPBackup' })
+	res.render('configuracao', { title: 'Configuração' })
 });
 
 module.exports = router;
