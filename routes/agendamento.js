c5e5b76b036616b9f103904c852a72dca0d8ef8e
David Alves <david27alves@gmail.com>
2019-10-26 21:48:29 -0300

    Refatorado

--- routes/agendamento.js
+++ routes/agendamento.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	res.render('agendamento', { title: 'Agendamento' })
+	res.render('agendamento', { title: 'Agendamento', select: true });
 });
 
 module.exports = router;
